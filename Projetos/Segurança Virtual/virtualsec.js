61ee2015316ad5eeed10c9fdbfca63cf7135d7b1
dansilx <danilosilveira@msn.com>
2024-07-01 17:24:39 -0300

    salvando novo projeto

---
+++ Projetos/Segurança Virtual/virtualsec.js
@@ -0,0 +1,6 @@
+function verificarEntrada() {
+    let nome = document.getElementById("nomeInput").value
+    let convidado = document.getElementById("convidadoInput").value
+    
+    document.getElementById("resultado").innerText
+}
